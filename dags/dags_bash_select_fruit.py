c3d30587e1495a8c865fa3bbddddf0bb1d4dee12
becha <remnant1@daum.net>
2025-12-29 11:27:16 +0900

    select_fruit 수행하는 dag 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import pendulum
 import datetime
-from airflow.operators.bash import BashOperator
+from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_select_fruit",
